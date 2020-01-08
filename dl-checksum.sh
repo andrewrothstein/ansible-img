bb38b91eb1c37821dd80ba56cd687c4256bbc877
Andrew Rothstein <andrew.rothstein@gmail.com>
2020-01-08 16:35:58 -0500

    initial commit

---
+++ dl-checksum.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+DIR=~/Downloads
+MIRROR=https://github.com/genuinetools/img/releases/download
+
+dl()
+{
+    local ver=$1
+    local os=$2
+    local arch=$3
+    local platform="${os}-${arch}"
+    local shafile="img-${platform}.sha256"
+    local url=$MIRROR/$ver/$shafile
+    printf "    # %s\n" $url
+    printf "    %s: sha256:%s\n" $platform $(curl -sSL $url | awk '{print $1}')
+}
+
+dl_ver() {
+    local ver=$1
+    printf "  %s:\n" $ver
+    dl $ver linux amd64
+}
+
+dl_ver ${1:-v0.5.7}
